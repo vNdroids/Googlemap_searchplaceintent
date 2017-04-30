32af6f836e6618cc1f674c23fe9ed91091b426c9
vikas2dx@gmail.com <coolvikas123>
2017-04-30 23:43:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/googlemap_searchplaceintent/MapsActivity.java
@@ -0,0 +1,114 @@
+package com.example.user.googlemap_searchplaceintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 01;
+    private static final String TAG = "TAG";
+    GoogleApiClient mGoogleApiClient = null;
+    String place_name;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        initGoogleApiClient();
+        findPlace();
+    }
+
+    private void initGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+    public void findPlace() {
+        try {
+            AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                    .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                    .build();
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .setFilter(typeFilter)
+                    .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error./
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Toast.makeText(this, "Place:" + place.getAddress(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onActivityResult: placeid" + place.getId());
+                place_name = (String) place.getAddress();
+
+                Places.GeoDataApi.getPlaceById(mGoogleApiClient, place.getId())
+                        .setResultCallback(new ResultCallback<PlaceBuffer>() {
+                            @Override
+                            public void onResult(PlaceBuffer places) {
+                                if (places.getStatus().isSuccess()) {
+                                    final Place myPlace = places.get(0);
+                                    LatLng queriedLocation = myPlace.getLatLng();
+                                    Log.v("Latitude is", "" + queriedLocation.latitude);
+                                    Log.v("Longitude is", "" + queriedLocation.longitude);
+
+                                    LatLng sydney = new LatLng(queriedLocation.latitude, queriedLocation.longitude);
+                                    mMap.addMarker(new MarkerOptions().position(sydney).title(place_name));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                                }
+                                places.release();
+                            }
+                        });
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+}
